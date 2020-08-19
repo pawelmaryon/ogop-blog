1130d1da6035f4264bafa360604878d903239b72
pawelmaryon <maryonpawel@gmail.com>
2020-08-19 14:14:08 +0100

    articles view and controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
