0e6d0164fa5a85eb1de9d54f6761d77a7c538f42
pawelmaryon <maryonpawel@gmail.com>
2020-08-19 22:46:32 +0100

    article routes done and index view, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/destroy'
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/destroy'
+  resources :articles
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
