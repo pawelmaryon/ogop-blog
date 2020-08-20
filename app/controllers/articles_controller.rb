ac0e690d2044bb9296dd43d34f7c1d53b3f7f73a
pawelmaryon <maryonpawel@gmail.com>
2020-08-20 22:09:17 +0100

    unwanted code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,18 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to @article
   end
 
   def edit
@@ -19,5 +25,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
