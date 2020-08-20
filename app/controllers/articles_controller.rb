7bc8ae9aadfa058380433a551d7507153e0609fb
pawelmaryon <maryonpawel@gmail.com>
2020-08-20 22:35:46 +0100

    show new create beindone

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,32 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+
+    retirect_to @article
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
